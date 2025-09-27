666594d07afbe5d8b992cbbd02d7fae66391c22e
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2025-09-26 21:30:20 -0300

    Arrumei os operadores e os floats

--- JokeyCPP/Jokey/FloatingPoint.cpp
+++ JokeyCPP/Jokey/FloatingPoint.cpp
@@ -5,6 +5,10 @@ FloatingPoint::FloatingPoint() : Automato(0, { 3 }) {
 		addTransiction(0, c, 1);
 	}
 
+	for (char c = '0'; c <= '9'; c++) {
+		addTransiction(1, c, 1);
+	}
+
 	addTransiction(1, '.', 2);
 
 	for (char c = '0'; c <= '9'; c++) {
--- JokeyCPP/Jokey/Operator.cpp
+++ JokeyCPP/Jokey/Operator.cpp
@@ -1,6 +1,6 @@
 #include "Operator.h"
 
-Operator::Operator() : Automato(0, {1, 2, 3, 4, 5}) {
+Operator::Operator() : Automato(0, {1, 2, 3, 4, 5, 6}) {
 	addTransiction(0, '@', 1);
 	addTransiction(0, '#', 1);
 	addTransiction(0, '.', 1);
@@ -28,8 +28,8 @@ Operator::Operator() : Automato(0, {1, 2, 3, 4, 5}) {
 	addTransiction(2, '=', 3);
 
 	addTransiction(0, '&', 4);
-	addTransiction(0, '|', 4);
+	addTransiction(0, '|', 5);
 
 	addTransiction(4, '&', 5);
-	addTransiction(4, '|', 5);
+	addTransiction(5, '|', 6);
 }
